68787428ac404b5d935a77b2739a061bae4f4187
Matthew Stoodley <matt.storyfeet@gmail.com>
2023-02-28 20:28:27 +0000

    now can make the sums work well

--- src/Main.elm
+++ src/Main.elm
@@ -1,25 +1,54 @@
 module Main exposing (..)
 
 import Browser as Br
+import Random as R
+import Html 
+
 
 type Msg =
         DoThing
+        | Gen (List Question)
 
+type alias Question = Html.Html Msg
 
-init _ = ({},Cmd.none)
+type alias Model = 
+        {
+                sums : List Question
+        }
 
-view _ = 
+type alias IntRange = (Int,Int)
+
+addGen : IntRange -> IntRange -> R.Generator (Int , Int)
+addGen (a1,a2) (b1,b2) =
+        R.map2 (\a b -> (a,b)) (R.int a1 a2) (R.int b1 b2)  
+
+addMake : (Int , Int) -> Question
+addMake (a ,b) = Html.text (String.fromInt a ++ " + " ++ String.fromInt b ++ "=") 
+
+init : {} -> (Model,Cmd Msg)
+init _ = ({sums = []},R.generate (\l -> Gen (List.map addMake l))(addGen (2,20) (2,20)|> R.list 20))
+
+view : Model -> Br.Document Msg
+view pg = 
         { title = "Poo"
-        , body = []
+        , body = 
+                [ Html.h1 [] [Html.text "Math Worksheet" ] 
+                , Html.div [] pg.sums
+                ]
         }
 
 
-update _ _ = ({}, Cmd.none)
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update ms md = 
+        case ms of 
+                Gen l -> ({md | sums = l},Cmd.none) 
+                _ -> (md ,Cmd.none)
 
 -- Sub and Cmd are in Core Platform, that's why I couldn't find them
 subscriptions _ = Sub.none
 
-main : Program (List Int) {} Msg
+main : Program {} Model Msg
 main = Br.document {
         init = init
         , view = view
